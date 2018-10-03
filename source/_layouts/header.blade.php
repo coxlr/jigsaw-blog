7a23a52f5a1275524c8eedd8cd1e0ac12527f062
Lee Cox <lee_cox@sky.com>
2018-10-03 12:14:03 -0700

    added layout and vue integration

---
+++ source/_layouts/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container mx-auto">
+        <nav class="flex items-center justify-between p-4 mb-8 border-b">
+                <div class="flex items-center">
+                    <a href="/" class="nav-brand">
+                        <svg class="w-16" viewBox="0 0 162 157" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Personal-Logo">
+                                    <ellipse id="Outer-Circle" fill="#CBCBCB" cx="81" cy="78.5" rx="81" ry="78.5"></ellipse>
+                                    <ellipse id="Inner-Cirle" stroke="#FFFFFF" fill="#FFFFFF" cx="81" cy="79" rx="73" ry="71"></ellipse>
+                                    <text id="Initials" font-family="Nunito-Black, Nunito" font-size="70" font-weight="700" fill="#959595">
+                                        <tspan x="32" y="103">LC</tspan>
+                                    </text>
+                                </g>
+                            </g>
+                        </svg>                    
+                    </a>
+                </div>
+                <div class="flex items-center">
+                    <a href="/" class="text-lg font-bold text-grey-darkest no-underline hover:underline hover:text-orange">About</a>
+                    <a href="/" class="text-lg font-bold text-grey-darkest no-underline hover:underline hover:text-orange ml-4">Contact</a>
+                </div>
+            </nav>
+    </div>
+</header>
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -6,7 +6,16 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body>
-        @yield('body')
+    <body class="bg-grey-lighter">
+		@include('_layouts.header')
+		<div class="container mx-auto px-4 md:px-2 md:px-0" id="app">
+        	<section class="flex flex-col-reverse sm:flex-row">
+        		@yield('body')
+        	</section>
+       	</div>
+       	@include('_layouts.footer')
+
+        <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
+       	<script defer src="https://use.fontawesome.com/releases/v5.2.0/js/all.js" integrity="sha384-4oV5EgaV02iISL2ban6c/RmotsABqE4yZxZLcYMAdG7FAPsyHYAPpywE9PJo+Khy" crossorigin="anonymous"></script>
     </body>
 </html>
